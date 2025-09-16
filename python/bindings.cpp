e3a45c4e47d195d68b1327b5f359e00f4c13d35f
Yuhao Jiang <32806424+Yuhaooou@users.noreply.github.com>
2025-09-16 18:23:51 +0800

    test

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,12 +1,12 @@
 // File: python/bindings.cpp
 #include <pybind11/pybind11.h>
-#include <test/TestCFRankQ2FBlindDecon2D.h>
+#include <test/TestFRankETextureInpainting.h>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(pyroptlib, m) {
     m.doc() = "My awesome C++ library exposed to Python";
 
-    m.def("testCFRankQ2FBlindDecon2D", &testCFRankQ2FBlindDecon2D, "A function that tests CFRankQ2FBlindDecon2D");
+    m.def("test", &testFRankETextureInpainting, "A function that tests CFRankQ2FBlindDecon2D");
 
 }
